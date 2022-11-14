def739fb2443e10eeff8c7276cbc7e8b2f81f2d1
Dario-Miccinelli <poduzzo@hotmail.com>
2022-11-15 00:01:27 +0100

    end;

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,49 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
-    }
-  })    
+        errorMsg: '',
+        newMsg: '',
+        list: [
+
+            {
+               text: 'Fare esercizio del pomeriggio',
+               done: false,
+            },
+            {
+               text: 'Fare la spesa',
+               done: false,
+            },
+            {
+               text: 'Andare in palestra',
+               done: false,
+            },
+
+         ],
+    },
+    methods: {
+        removeItem(index) {
+            console.log(index);
+            this.errorMsg = '';
+            if(this.list[index].done) {
+                this.list.splice(index, 1)
+            }else {
+                this.errorMsg = 'Attenzione! Non puoi eliminare una task se non l\'hai ancora completata'
+            }
+        },
+        createItem() {
+            this.errorMsg = '';
+    
+            if(this.newMsg.length < 5) {
+                this.errorMsg = 'Attenzione! Il messaggio deve avere almeno 5 caratteri.';
+            }else {
+                const newItem = {
+                    text: this.newMsg,
+                    done: false
+                }
+                this.list.unshift(newItem);
+                this.newMsg = '';
+            }
+        }
+    },
+  
+  })
